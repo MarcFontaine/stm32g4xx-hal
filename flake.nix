0c0a7e682c3195f5068d5229fc57080b651f785d
MarcFontaine <MarcFontaine@users.noreply.github.com>
2026-06-30 23:37:58 +0200

    Fix flake for arm target

--- flake.nix
+++ flake.nix
@@ -6,26 +6,43 @@
     };
     nixpkgs.url = "nixpkgs/nixos-26.05";
   };
-
-  outputs = { self, fenix, nixpkgs }: {
+  outputs = { self, fenix, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ fenix.overlays.default ];
+    };
+    toolchain = with fenix.packages.${system}; combine [
+      minimal.cargo
+      minimal.rustc
+      targets.thumbv7em-none-eabihf.latest.rust-std
+    ];
+  in
+    {
+    test = fenix.packages.x86_64-linux;
     packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({ pkgs, ... }: {
-          nixpkgs.overlays = [ fenix.overlays.default ];
-          environment.systemPackages = [
-            (pkgs.fenix.complete.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-            pkgs.rust-analyzer-nightly
-          ];
-        })
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      name = "rust-devshell";
+
+      buildInputs = with pkgs; [
+        toolchain
+        pkgs.probe-rs-tools
+        pkgs.flip-link
       ];
+
+#      RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+#      RUSTFLAGS = [ "-C" "linker=${pkgs.stdenv.cc.targetPrefix}ld" ];
+      CARGO_BUILD_TARGET = "thumbv7em-none-eabihf";
+      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER =   let
+              inherit (pkgs.pkgsCross.aarch64-multiplatform.stdenv) cc;
+            in
+       "${cc}/bin/${cc.targetPrefix}cc";
+
+      shellHook = ''
+        echo "Rust DevShell aktiv!"
+        echo "Rust-Version: $(rustc --version)"
+      '';
     };
   };
 }
