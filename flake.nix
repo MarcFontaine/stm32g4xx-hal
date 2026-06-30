d764b9fab655b547d71421330ac73d930e734302
MarcFontaine <MarcFontaine@users.noreply.github.com>
2026-06-30 23:35:18 +0200

    Add flake enable nix develop

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, fenix, nixpkgs }: {
+    packages.x86_64-linux.default = fenix.packages.x86_64-linux.minimal.toolchain;
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [ fenix.overlays.default ];
+          environment.systemPackages = [
+            (pkgs.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            pkgs.rust-analyzer-nightly
+          ];
+        })
+      ];
+    };
+  };
+}
